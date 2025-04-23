bf34c90b3472f5e204cf69cde8fbb20d4e513604
Mandus Börjesson <borjessonmandus@gmail.com>
2025-04-23 21:06:24 +0200

    Start building the UI using tkinter

--- thermal.py
+++ thermal.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+import tkinter as tk
+from PIL import Image, ImageTk, ImageOps
+
 
 def yuyv422_to_gray(img):
     return img[..., 0]
@@ -21,87 +24,117 @@ def mouse_callback(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE:
         cursor_x, cursor_y = x, y
 
+class ThermalCamera():
+    def __init__(self, path):
+        self._path = path
+        self._size = (256, 384)
+        self._cam = None
+
+    def init_cam(self):
+        cap = cv2.VideoCapture(self._path)
+        if not cap.isOpened():
+            return False
+
+        # Set the camera resolution to 256x384 explicitly
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, self._size[0])
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self._size[1])
+        cap.set(cv2.CAP_PROP_CONVERT_RGB, 0)
+
+        # Fetch undecoded RAW video streams
+        cap.set(cv2.CAP_PROP_FORMAT, -1)
+
+        self._cam = cap
+        return True
+
+    def read(self):
+        if not self._cam:
+            if not self.init_cam():
+                print("Failed to initialize camera")
+                return
+
+        ret, frame = self._cam.read()
+        if not ret:
+            self._cam = None
+            print("Failed to read camera")
+            return None
+
+        # Split the frame into upper and lower halves
+        h_half = self._size[1]//2
+        upper_frame = frame[:h_half, :, :]
+        lower_frame = frame[h_half:, :, :]
+
+        return upper_frame, lower_frame
+
+    def close(self):
+        if self._cam:
+            self._cam.release()
+
+
+cap = ThermalCamera("/dev/video-infiray")
 
 selected_cmap = "COLORMAP_MAGMA"
 temp_scale = 4
 
-cv2.namedWindow("Grayscale", cv2.WINDOW_NORMAL)
 # cv2.resizeWindow("Grayscale", 800, 600)
 
-
-# Set the mouse callback function for both
-cv2.setMouseCallback("Grayscale", mouse_callback)
 
 # Initialize cursor position
 cursor_x, cursor_y = 0, 0
 
-cap = cv2.VideoCapture("/dev/video-infiray")
 
-# Set the camera resolution to 256x384 explicitly
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 256)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 384)
-cap.set(cv2.CAP_PROP_CONVERT_RGB, 0)
+def handle_keypress(event):
+    """Print the character associated to the key pressed"""
+    print(event.char)
 
-# Fetch undecoded RAW video streams
-cap.set(cv2.CAP_PROP_FORMAT, -1)
+def update_camera_feed(webcam, widget):
+
+    # Capture the video frame by frame
+    ret = webcam.read()
+    if not ret:
+        widget.after(5000, update_camera_feed, webcam, widget)
+        return
+
+    # Split the frame into upper and lower halves
+    upper_frame, _ = ret
+
+    # Convert the upper frame to grayscale
+    gray_upper = yuyv422_to_gray(upper_frame)
+    opencv_image = gray_upper
+
+    # Apply the selected color map to the grayscale image
+    opencv_image = cv2.applyColorMap(gray_upper, cv2.COLORMAP_MAGMA)
+    captured_image = Image.fromarray(opencv_image)
+
+    widget_size = (widget.winfo_width(), widget.winfo_height())
+    captured_image = ImageOps.contain(captured_image, widget_size, method=Image.Resampling.NEAREST)
+
+    photo_image = ImageTk.PhotoImage(image=captured_image)
+
+    # Displaying photoimage in the label
+    widget.photo_image = photo_image
+
+    # Configure image in the label
+    widget.configure(image=photo_image)
+
+    widget.after(1, update_camera_feed, webcam, widget)
 
 def main():
-    if not cap.isOpened():
-        raise ValueError("Cannot open camera")
+    window = tk.Tk()
+    # window["bg"] = "black"
+    window.bind('<q>', lambda e: window.quit())
 
+    camera_feed = tk.Label(master=window, bg="black", borderwidth=0, highlightthickness=0)
+    camera_feed.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+    frame2 = tk.Frame(master=window, width=100, bg="yellow")
+    frame2.pack(fill=tk.BOTH, side=tk.LEFT)
+
+    update_camera_feed(cap, camera_feed)
     try:
-        flip_x = False
-        flip_y = False
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-
-            # Split the frame into upper and lower halves
-            upper_frame = frame[:192, :, :]
-            lower_frame = frame[192:, :, :]
-
-            # Convert the upper frame to grayscale
-            gray_upper = yuyv422_to_gray(upper_frame)
-
-            # Extract the raw non-normalized sensor values from the lower frame
-            gray16le_lower = yuyv422_to_gray16le(lower_frame)
-
-            # Convert temperature data to Celsius
-            temperature_c = kelvin_to_celsius(gray16le_lower / 64)
-            temperature = temperature_c[cursor_y//temp_scale, cursor_x//temp_scale]
-            temperature_text = f" {temperature:.1f} C"
-
-            if flip_x and flip_y:
-                gray_upper = cv2.flip(gray_upper, -1)
-            elif flip_x:
-                gray_upper = cv2.flip(gray_upper, 0)
-            elif flip_y:
-                gray_upper = cv2.flip(gray_upper, 1)
-
-            # Apply the selected color map to the grayscale image
-            colormap_idx = getattr(cv2, selected_cmap)
-            color_mapped_gray = cv2.applyColorMap(gray_upper, colormap_idx)
-
-            color_mapped_gray_resized = cv2.resize(color_mapped_gray, None, fx=temp_scale, fy=temp_scale,
-                                                   interpolation=cv2.INTER_NEAREST_EXACT)
-            cv2.circle(color_mapped_gray_resized, (cursor_x, cursor_y), 2, (255, 255, 255), 1)
-            cv2.putText(color_mapped_gray_resized, temperature_text, (cursor_x, cursor_y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-
-            cv2.imshow("Grayscale", color_mapped_gray_resized)
-
-            key = cv2.waitKey(1)
-            if key == ord("q"):
-                break
-            if key == ord("x"):
-                flip_x = not flip_x
-            if key == ord("y"):
-                flip_y = not flip_y
-
+        window.mainloop()
     finally:
-        cap.release()
-        cv2.destroyAllWindows()
+        cap.close()
+
 
 if __name__ == "__main__":
     main()
